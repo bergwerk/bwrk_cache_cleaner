16f872a98f77848999961c1516255915d9791b00
Georg Dümmler <gd@bergwerk.ag>
2015-10-23 12:04:34 +0200

    Fix Merge Conflicts

--- Classes/Command/CacheCommandController.php
+++ Classes/Command/CacheCommandController.php
@@ -2,11 +2,42 @@
 
 namespace BERGWERK\BwrkCacheCleaner\Command;
 
+/***************************************************************
+ *  Copyright notice
+ *
+ *  (c) 2015 Georg Dümmler <gd@bergwerk.ag>
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ *
+ * @author    Georg Dümmler <gd@bergwerk.ag>
+ * @package    TYPO3
+ * @subpackage    bwrk_cache_cleaner
+ ***************************************************************/
+
 use BERGWERK\BwrkCacheCleaner\Domain\Model\LogEntry;
 use BERGWERK\BwrkCacheCleaner\Service\CacheApiService;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Extbase\Mvc\Controller\CommandController;
 
+/**
+ * Class CacheCommandController
+ * @package BERGWERK\BwrkCacheCleaner\Command
+ */
 class CacheCommandController extends CommandController
 {
 
@@ -19,17 +50,44 @@ class CacheCommandController extends CommandController
      */
     protected $logEntryRepository;
 
+    /**
+     * Constructor
+     */
     public function __construct()
     {
         $this->cacheApiService = GeneralUtility::makeInstance('BERGWERK\\BwrkCacheCleaner\\Service\\CacheApiService');
     }
 
     /**
-     * @param bool|false $system
+     * @param string $type
      */
-    public function runCommand($system = false)
+    public function runCommand($type = '')
     {
-        $this->clearCache($system);
+        if ($type == 'page') {
+            $this->cacheApiService->clearPageCache();
+
+            echo "Page cache cleared successfully!\n";
+        } else if ($type == 'system') {
+            $this->cacheApiService->clearSystemCache();
+
+            echo "System cache cleared successfully!\n";
+        } else if ($type == 'configuration') {
+            $this->cacheApiService->clearConfigurationCache();
+
+            echo "Configuration cache cleared successfully!\n";
+        } else if ($type == 'opc') {
+            $this->cacheApiService->clearAllActiveOpcodeCache();
+
+            echo "OPC cache cleared successfully!\n";
+        } else if ($type == 'except_page') {
+            $this->cacheApiService->clearAllExceptPageCache();
+
+            echo "All caches except page cache cleared successfully!\n";
+        } else {
+            $this->clearCache();
+
+            echo "All caches cleared successfully!\n";
+        }
     }
 
     /**
@@ -41,16 +99,14 @@ class CacheCommandController extends CommandController
     }
 
     /**
-     * @param string $system
+     * Clear Cache
      */
-    private function clearCache($system)
+    private function clearCache()
     {
         $this->cacheApiService->clearAllActiveOpcodeCache();
-
-        if($system == true)
-        {
-            $this->cacheApiService->clearSystemCache();
-        }
+        $this->cacheApiService->clearSystemCache();
+        $this->cacheApiService->clearConfigurationCache();
+        $this->cacheApiService->clearPageCache();
 
         $this->cacheApiService->clearAllCaches(false);
     }
--- Classes/Service/CacheApiService.php
+++ Classes/Service/CacheApiService.php
@@ -1,11 +1,43 @@
 <?php
 namespace BERGWERK\BwrkCacheCleaner\Service;
 
+/***************************************************************
+ *  Copyright notice
+ *
+ *  (c) 2015 Georg Dümmler <gd@bergwerk.ag>
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ *
+ * @author    Georg Dümmler <gd@bergwerk.ag>
+ * @package    TYPO3
+ * @subpackage    bwrk_cache_cleaner
+ ***************************************************************/
+
 use TYPO3\CMS\Core\Authentication\BackendUserAuthentication;
 use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 
-class CacheApiService {
+/**
+ * Class CacheApiService
+ * @package BERGWERK\BwrkCacheCleaner\Service
+ */
+class CacheApiService
+{
     /**
      * @var \TYPO3\CMS\Core\DataHandling\DataHandler
      */
@@ -20,6 +52,9 @@ class CacheApiService {
     protected $installToolClearCacheService;
 
 
+    /**
+     * Constructor
+     */
     public function __construct()
     {
         $this->dataHandler = GeneralUtility::makeInstance('TYPO3\\CMS\\Core\\DataHandling\\DataHandler');
@@ -41,9 +76,9 @@ class CacheApiService {
      * @param bool $hard
      * @return void
      */
-    public function clearAllCaches($hard = FALSE) {
-        if(!$hard)
-        {
+    public function clearAllCaches($hard = FALSE)
+    {
+        if (!$hard) {
             $this->dataHandler->clear_cacheCmd('all');
         } else {
             GeneralUtility::rmdir(PATH_site . 'typo3temp/Cache', TRUE);
@@ -67,48 +102,53 @@ class CacheApiService {
     }
 
     /**
-     * Clear the page cache.
+     * Use this to clear the Page Cache.
      *
      * @return void
      */
-    public function clearPageCache() {
+    public function clearPageCache()
+    {
         $this->dataHandler->clear_cacheCmd('pages');
     }
+
     /**
-     * Clears the configuration cache.
+     * Use this to clear the configuration cache.
      *
      * @return void
      */
-    public function clearConfigurationCache() {
+    public function clearConfigurationCache()
+    {
         $this->dataHandler->clear_cacheCmd('temp_cached');
     }
+
     /**
-     * Clear the system cache
+     * Use this to clear the system cache
      *
      * @return void
      */
-    public function clearSystemCache() {
+    public function clearSystemCache()
+    {
         $this->dataHandler->clear_cacheCmd('system');
     }
+
     /**
-     * Clears the opcode cache.
+     * Use this to clear the opcode cache.
      *
-     * @param string|NULL $fileAbsPath The file as absolute path to be cleared
-     *                                 or NULL to clear completely.
-     *
+     * @param string|NULL $fileAbsPath
      * @return void
      */
-    public function clearAllActiveOpcodeCache($fileAbsPath = NULL) {
+    public function clearAllActiveOpcodeCache($fileAbsPath = NULL)
+    {
         $this->clearAllActiveOpcodeCacheWrapper($fileAbsPath);
     }
+
     /**
      * Clear all caches except the page cache.
-     * This is especially useful on big sites when you can't
-     * just drop the page cache.
      *
      * @return array with list of cleared caches
      */
-    public function clearAllExceptPageCache() {
+    public function clearAllExceptPageCache()
+    {
         $out = array();
         $cacheKeys = array_keys($GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']);
         $ignoredCaches = array('cache_pages', 'cache_pagesection');
@@ -124,15 +164,22 @@ class CacheApiService {
         }
         return $toBeFlushed;
     }
+
     /**
-     * Clears the opcode cache. This just wraps the static call for testing purposes.
+     * Use this to clear the opcode cache.
      *
-     * @param string|NULL $fileAbsPath The file as absolute path to be cleared
-     *                                 or NULL to clear completely.
-     *
+     * @param string|NULL $fileAbsPath
      * @return void
      */
-    protected function clearAllActiveOpcodeCacheWrapper($fileAbsPath) {
-        \TYPO3\CMS\Core\Utility\OpcodeCacheUtility::clearAllActive($fileAbsPath);
+    protected function clearAllActiveOpcodeCacheWrapper($fileAbsPath)
+    {
+        if(version_compare(TYPO3_version, '7.4.0', '>='))
+        {
+            /** @var \TYPO3\CMS\Core\Service\OpcodeCacheService $opcodeCacheService */
+            $opcodeCacheService = GeneralUtility::makeInstance(\TYPO3\CMS\Core\Service\OpcodeCacheService::class);
+            $opcodeCacheService->clearAllActive($fileAbsPath);
+        } else {
+            \TYPO3\CMS\Core\Utility\OpcodeCacheUtility::clearAllActive($fileAbsPath);
+        }
     }
 }
